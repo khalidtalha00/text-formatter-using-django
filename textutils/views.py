877fa70bfddd7863349a25c381b3018bed9f3fbc
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2024-04-24 23:07:02 +0530

    Add files via upload

---
+++ textutils/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+
+def analyze(request):
+    djtext=request.POST.get('text','default')
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremove=request.POST.get('newlineremove','off')
+    extraspaceremove=request.POST.get('extraspaceremove','off')
+
+    if removepunc=='on':
+        analyzed = ""
+        punctuations= '''.…/‘“”<>{}[]()—–;:,!?.'''
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed+char
+        params={'purpose':'Remove Punctuations','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif (fullcaps=="on"):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params={'purpose':'Change to Uppercase','analyzed_text':analyzed}
+
+        return render(request,'analyze.html',params)
+    elif newlineremove=="on":
+     analyzed=""
+     for char in djtext:
+      if char !="\n" and char!="\r":
+       analyzed=analyzed+char
+     params={'purpose':'Remove newline','analyzed_text':analyzed}
+
+     return render(request,"analyze.html",params)
+    elif extraspaceremove=='on':
+        analyzed = ""
+        length = len(djtext)
+        for index in range(length):
+            if not (djtext[index] == " " and index + 1 < length and djtext[index + 1] == " "):
+                analyzed += djtext[index]
+
+        params={'purpose':'Remove Extra space','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    else:
+        return HttpResponse("ERROR")
+
